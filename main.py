f2478a0a87eb189719b8e088ba92dddbed0297d0
tobi <tobibags19@gmail.com>
2023-05-10 09:09:40 -0500

    Implement input functions for CLI usage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import webbrowser
 from fpdf import FPDF
 
+
 class Bill:
     """
     Object containing data about a bill, such as
@@ -71,12 +72,21 @@ class PdfReport:
         webbrowser.open(self.filename)
 
 
-the_bill = Bill(amount=120, period="April 2023")
-john = Flatmate(name="John", days_in_house=20)
-marry = Flatmate(name="Marry", days_in_house=25)
+amount = float(input("Hey, enter the bill amount: "))
+period = input("What is the billing period? E.g. December 2022: ")
 
-print("John pays: ", john.pays(bill=the_bill, flatmate2=marry))
-print("Marry pays: ", marry.pays(bill=the_bill, flatmate2=john))
+name_1 = input("Please, enter your name? ")
+days_in_house_1 = int(input(f"How many days did {name_1} stay in the house during these bill period? "))
 
-pdf_report = PdfReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
+name_2 = input("Please, enter your flatmate's name? ")
+days_in_house_2 = int(input(f"How many days did {name_2} stay in the house during these bill period? "))
+
+the_bill = Bill(amount, period)
+flatmate_1 = Flatmate(name_1, days_in_house_1)
+flatmate_2 = Flatmate(name_2, days_in_house_2)
+
+print(f"{flatmate_1.name} pays: ", flatmate_1.pays(the_bill, flatmate_2))
+print(f"{flatmate_2.name} pays: ", flatmate_2.pays(the_bill, flatmate_1))
+
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate_1, flatmate_2, the_bill)
