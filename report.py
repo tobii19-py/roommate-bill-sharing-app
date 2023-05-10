c1f9e899d912f611fd773b0f60e3214abfa797c8
tobi <tobibags19@gmail.com>
2023-05-10 09:13:32 -0500

    Refactored main.py into 3 py files

---
+++ report.py
@@ -0,0 +1,46 @@
+import webbrowser
+from fpdf import FPDF
+
+
+class PdfReport:
+    """
+    Creates a pdf file that contains data about
+    the flatmates such as names, share and
+    billing period
+    """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+
+        flatmate1_pay = str(round(flatmate1.pays(bill, flatmate2), 2))
+        flatmate2_pay = str(round(flatmate2.pays(bill, flatmate1), 2))
+
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # Add icon
+        pdf.image("house.png", w=30, h=30)
+
+        # Insert a title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt="Flatmates Bill", border=0, align="C", ln=1)
+
+        # Insert Period label and value
+        pdf.set_font(family="Times", size=14, style='B')
+        pdf.cell(w=100, h=40, txt="Period:", border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # Insert the flatmate number 1's name and amount due
+        pdf.set_font(family="Times", size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate1_pay, border=0, ln=1)
+
+        # Insert the flatmate number 2's name and amount due
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
+
+        pdf.output(self.filename)
+
+        webbrowser.open(self.filename)
