6fb16b2897670d754518d4c3dbbdbb3b159d0eaf
tobi <tobibags19@gmail.com>
2023-05-10 08:34:22 -0500

    Automate opening pdf bill

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import webbrowser
 from fpdf import FPDF
 
 class Bill:
@@ -66,6 +67,8 @@ class PdfReport:
         pdf.cell(w=150, h=25, txt=flatmate2_pay, border=0, ln=1)
 
         pdf.output(self.filename)
+
+        webbrowser.open(self.filename)
 
 
 the_bill = Bill(amount=120, period="April 2023")
